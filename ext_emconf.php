ef18322e29965ae3adfcb42200398497f536c140
jbartels <j.bartels@arcor.de>
2017-12-11 21:15:03 +0100

    Compatibility for Typo3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'memcacheddiag2',
 	'description' => 'Displays diagnosis information about memcache',
 	'category' => 'module',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'author' => 'Jan Bartels',
 	'author_email' => 'j.bartels@arcor.de',
 	'state' => 'stable',
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '7.6.0-8.7.99',
 			'php' => '5.5.0-7.0.99',
 		),
 		'conflicts' => array(
